860f9ea336084fc3a5e603ea9eb6bfd2cb15b914
Harm Manders <harmmanders@gmail.com>
2023-02-23 22:34:59 +0100

    v1 dicecloud

--- src/dicecloud/character.js
+++ src/dicecloud/character.js
@@ -48,11 +48,20 @@ class DiceCloudCharacter extends Character {
 
 
 	parseAvatar() {
+		if (this.isV1) {
+			return null;
+		}
 		const avatar_url = document.querySelector('.v-avatar img')?.src;
 		return avatar_url ?? null;
 	}
 
 	async parseLevel() {
+		if (this.isV1) {
+			const level_v1 = document.querySelector('[name="journal"] .containerName')?.textContent.trim();
+			const level = parseInt(level_v1.replace(/\D*/, ""))
+			return level ?? null;
+			
+		}
 		const container = document.querySelector('.class-details .v-card__title')
 		if (!container) {
 			document.querySelectorAll('.v-tab')[6].click()
@@ -62,45 +71,74 @@ class DiceCloudCharacter extends Character {
 		
 		const level_str = document.querySelector('.class-details .v-card__title').textContent.trim()
 		const level = parseInt(level_str.replace(/\D*/, ""));
-		console.log("in timeout", level);
 		return level ?? null;
 	}
 
 	parseStat(stat) {
-		const containers = document.querySelectorAll('.stat');
+		let container_selector = '.stat'
+		let stat_name_selector = '.v-card .layout .name'
+		let stat_value_selector = '.v-card .layout .value'
+		if (this.isV1) {
+			container_selector = '.stat-card'
+			stat_name_selector = '.paper-font-subhead'
+			stat_value_selector = '.numbers'
+		}
+		let containers = document.querySelectorAll(container_selector);
 		
 		let parsedStat;
 		for(const container of containers) {
-			const name = container?.querySelector('.v-card .layout .name')?.textContent;
+			const name = container?.querySelector(stat_name_selector)?.textContent;
 			if(name.trim() === stat) {
-				parsedStat = container?.querySelector('.v-card .layout .value')?.textContent;
+				parsedStat = container?.querySelector(stat_value_selector)?.textContent;
+				return parseInt(parsedStat) ?? null;
 			}
 		}
-		return parseInt(parsedStat) ?? null;
+		return null;
 	}
 
 	parseCheck(check) {
-		const containers = document.querySelectorAll('.check');
+		let container_selector = '.check'
+		let stat_name_selector = '.v-card .layout .name'
+		let stat_value_selector = '.v-card .layout div .v-btn .v-btn__content .value'
+		if (this.isV1) {
+			container_selector = '.stat-card'
+			stat_name_selector = '.paper-font-subhead'
+			stat_value_selector = '.numbers div'
+		}
+
+		const containers = document.querySelectorAll(container_selector);
 		
 		let parsedStat;
 		for(const container of containers) {
-			const name = container?.querySelector('.v-card .layout .name')?.textContent;
+			const name = container?.querySelector(stat_name_selector)?.textContent;
 			if(name.trim() === check) {
-				parsedStat = container?.querySelector('.v-card .layout div .v-btn .v-btn__content .value')?.textContent;
+				parsedStat = container?.querySelector(stat_value_selector)?.textContent;
+				return parseInt(parsedStat) ?? null;
 			}
 		}
-		return parseInt(parsedStat) ?? null;
+		return null;
 	}
 
 	parseMaxHitPoints() {
+		if (this.isV1) {
+			const hpField = document.querySelector('#hitPointSlider #input-1')
+			const Hp = hpField.getAttribute('max')
+			return parseInt(Hp) ?? null;
+		}
 		const Hp = document.querySelector('.health-bar .bar .value')?.textContent;
 		const parsedHp = Hp ? parseInt(Hp.split("/")[1]) : null;
 		return parsedHp ?? null;
 	}
 
 	parseAbilityScore(n) {
-		const container = document.querySelectorAll('.ability-scores .v-list .ability-list-tile')[n];
-		const parsedScore = container?.querySelector('.v-list-item__action div .v-btn .v-btn__content div .value span')?.textContent;
+		let container_selector = '.ability-scores .v-list .ability-list-tile'
+		let stat_selector = '.v-list-item__action div .v-btn .v-btn__content div .value span'
+		if (this.isV1) {
+			container_selector = '.stats .ability-mini-card'
+			stat_selector = '.numbers .stat'
+		}
+		const container = document.querySelectorAll(container_selector)[n];
+		const parsedScore = container?.querySelector(stat_selector)?.textContent;
 		return parseInt(parsedScore) ?? 0;
 	}
 }
