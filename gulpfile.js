8df768da850d2971ccf8df66381d25b4f92b81d9
Harm Manders <harmmanders@gmail.com>
2023-03-10 17:22:57 +0100

    gulp build folder

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 var gulp = require('gulp');
 var gulp_concat = require('gulp-concat');
 var gulp_clean = require('gulp-clean');
+const gulpClean = require('gulp-clean');
 
 const UTILS = [
 	'src/common/store.js',
@@ -11,32 +12,24 @@ const UTILS = [
 const TARGETS = {
 	'dndbeyond_character': [
 		...UTILS,
-		'src/dndbeyond/utils.js',
-		'src/models/character.js',
-		'src/dndbeyond/character.js',
-		'src/dndbeyond/content.js',
+		'src/content/dndbeyond/utils.js',
+		'src/content/models/character.js',
+		'src/content/dndbeyond/character.js',
+		'src/content/dndbeyond/content.js',
 	],
 	'dicecloud_character': [
 		...UTILS,
-		'src/models/character.js',
-		'src/dicecloud/character.js',
-		'src/dicecloud/content.js',
+		'src/content/models/character.js',
+		'src/content/dicecloud/character.js',
+		'src/content/dicecloud/content.js',
 	],
 	'harmlesskey_character': [
 		...UTILS,
-		'src/models/character.js',
-		'src/harmlesskey/character.js',
-		'src/harmlesskey/content.js',
+		'src/content/models/character.js',
+		'src/content/harmlesskey/character.js',
+		'src/content/harmlesskey/content.js',
 	],
 }
-
-const DNDBEYOND_CHARACTER = [
-	...UTILS,
-	'src/dndbeyond/utils.js',
-	'src/models/character.js',
-	'src/dndbeyond/character.js',
-	'src/dndbeyond/content.js',
-]
 
 const targets = {}
 for (const target in TARGETS) {
@@ -44,7 +37,7 @@ for (const target in TARGETS) {
 		[target]: () => {
 			return gulp.src(TARGETS[target])
 				.pipe(gulp_concat(`${target}.js`))
-				.pipe(gulp.dest('dist/'))
+				.pipe(gulp.dest('./dist/'))
 		}
 	}
 	targets[target] = task[target]
@@ -52,20 +45,58 @@ for (const target in TARGETS) {
 	gulp.task(target, targets[target]);
 }
 
+const build_dist = gulp.series(...Object.values(targets));
 
-const clean = () => {
-	return gulp.src('./dist/', { read: false, allowEmpty: true }).pipe(gulp_clean());
-}
+const copy_dist_nobuild = () =>
+	gulp.src("./dist/**").pipe(gulp.dest('./build/base/content/'));
 
-const build = async () => {
-	return gulp.series(...Object.values(targets))();
-}
+const clean_dist = () =>
+	gulp.src('./dist/', { read: false, allowEmpty: true }).pipe(gulp_clean());
 
-const watch = () => {
-	for (target in TARGETS) {
+const copy_dist = gulp.series(clean_dist, build_dist, copy_dist_nobuild);
+
+
+const clean_build = () => 
+	gulp.src('./build/', {read: false, allowEmpty: true}).pipe(gulpClean());
+
+const copy_css = () =>
+	gulp.src('./src/css/**').pipe(gulp.dest('./build/base/css/'))
+
+const copy_assets = () => 
+	gulp.src('./src/assets/**').pipe(gulp.dest('./build/base/assets/'))
+
+const copy_ext = () =>
+gulp.src('./src/extension/**').pipe(gulp.dest('./build/base/extension/'))
+
+const copy_lib = () =>
+	gulp.src('./lib/**').pipe(gulp.dest('./build/base/lib/'))
+
+const copy_background = () =>
+	gulp.src('./src/background.js').pipe(gulp.dest('./build/base/'))
+	
+const copy_index = () =>
+	gulp.src('./src/index.html').pipe(gulp.dest('./build/base/'))
+	
+const copy_manifest = () => 
+	gulp.src('manifest.json').pipe(gulp.dest('./build/base/'))
+
+
+const watch_targets = () => {
+	for (const target in TARGETS) {
 		gulp.watch(TARGETS[target], targets[target]);
 	}
 }
 
-exports.build = build;
-exports.default = gulp.series(clean, build, watch);
+const build_base = gulp.parallel([
+		copy_dist,
+		copy_ext,
+		copy_background,
+		copy_manifest,
+		copy_index,
+		copy_assets,
+		copy_css,
+		copy_lib
+	]);
+
+exports.build = gulp.series(clean_build, build_base);
+exports.default = gulp.series(clean_dist, build_base, watch);
