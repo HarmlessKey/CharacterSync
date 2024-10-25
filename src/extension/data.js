e91bcb76ba435af0379b5f2ff284c2e90e1ce856
Harm Manders <harmmanders@gmail.com>
2024-10-25 18:18:24 +0200

    remove old data vars

--- src/extension/data.js
+++ src/extension/data.js
@@ -1,8 +1,3 @@
-export const storage = await chrome.storage.sync.get();
-export const my_characters = Object.fromEntries(
-	Object.entries(storage).filter(([key]) => key !== "config")
-);
-
 export const getStorage = async () => {
 	return await chrome.storage.sync.get();
 };
